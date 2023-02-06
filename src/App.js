dbe7011d0b4717a4f376145306a36c76862bd640
libbyReeves <libby.reeves@hotmail.com>
2023-02-06 00:41:36 -0500

    made the base skeleton of the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+import "./components/NavBar";
+import NavBar from "./components/NavBar";
+import NoteAdd from "./components/NoteAdd";
+import Notebook from "./components/Notebook";
+import "./App.css";
 
-function App() {
+const firebaseConfig = {
+
+apiKey: "AIzaSyCw7NM3T1rbGcHjVepc_ISdLwfpQfNDCRs",
+
+authDomain: "crochet-notebook.firebaseapp.com",
+
+projectId: "crochet-notebook",
+
+storageBucket: "crochet-notebook.appspot.com",
+
+messagingSenderId: "855179883086",
+
+appId: "1:855179883086:web:b289320d2bd4d11173491f"
+
+};
+firebase.initializeApp(firebaseConfig);
+//
+
+//site components
+const App = () => {
+  const [noteBookData, setNoteBookData] = useState([]);
+
+  const updateNotes = () => {
+    firebase
+      .database()
+      .ref("notebook")
+      .on("child_added", (snapshot) => {
+        let note = {
+          id: snapshot.key,
+          title: snapshot.val().title,
+          description: snapshot.val().description,
+        };
+        let notebook = noteBookData;
+        notebook.push(note);
+        setNoteBookData([...noteBookData]);
+      });
+
+    firebase
+      .database()
+      .ref("notebook")
+      .on("child_removed", (snapshot) => {
+        let notebook = noteBookData;
+        notebook = noteBookData.filter((note) => note.id !== snapshot.key);
+        setNoteBookData(notebook);
+      });
+  };
+
+  useEffect(() => {
+    updateNotes();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <NavBar />
+      <div className="note-section">
+        <NoteAdd />
+        <Notebook notebook={noteBookData} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+    return (
+        <>
+            <header className="navBar">
+                <h2 className="title">Crochet Notebook</h2>
+            </header>
+        </>
+    );
+};
+
+export default NavBar;
